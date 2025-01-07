14aa341ae33c945f08b52a4d4ac05da15271028a
rubatista <inforubenbaptista@gmail.com>
2025-01-07 17:48:58 +0000

    feat: create sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -9,14 +11,17 @@ export default function DashboardLayout({
   return (
       <div className="h-screen flex">
         { /* LEFT */ }
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] bg-red-200">
-          <Link href="/" className="flex items-center justify-center gap-2">
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+          <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
             <Image src="/logo.png" alt="logo" width={32} height={32} />
             <span className="hidden lg:block">SchoolRubb</span>
           </Link>
+          <Menu/>
         </div>
         { /* RIGHT */ }
-        <div className="w-[84%] md:w-[92%] lg:w-[84%] bg-blue-200">right</div>
+        <div className="w-[84%] md:w-[92%] lg:w-[84%] bg-[#F7F8FA] overflow-scroll">
+          <Navbar />
+        </div>
       </div>
   );
 }
