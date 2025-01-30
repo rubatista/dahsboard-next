04159a077b06aeb3af8c8698df16fca944938a4b
rubatista <inforubenbaptista@gmail.com>
2025-01-30 00:31:21 +0000

    feat: create compoenent AttendanceChart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
@@ -23,7 +24,7 @@ const AdminPage = () => {
 
           {/*Attendance Chart*/}
           <div className="w-full lg:w-2/3 h-[450px]">
-
+            <AttendanceChart />
           </div>
         </div>
 
