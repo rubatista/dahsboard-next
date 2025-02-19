5150f587cb9212bd82bfb3ecc493b996d121f300
rubatista <inforubenbaptista@gmail.com>
2025-02-19 16:02:01 +0000

    feat: create new compoenet BigCalendar and style him, and add page for student, aprent and teacher

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,23 @@
+import EventCalendar from "@/components/EventCalendar"
+import Announcements from "@/components/Announcements"
+import BigCalendar from "@/components/BigCalendar"
+
 const StudentPage = () => {
   return (
-    <div className=''>StudentPage</div>
+    <div className='p-4 flex flex-col xl:flex-row gap-4'>
+      {/*Left*/}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/*Right*/}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
   )
 }
 
