8121c25bca788ac0ad7e547ed3c71b4fbd197866
rubatista <inforubenbaptista@gmail.com>
2025-03-01 00:00:48 +0000

    fix: fixed error card image

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -10,31 +10,27 @@ const SingleTeacherPage = () => {
                     {/* USER INFO CARD */}
                     <div className="bg-rubbSky py-6 px-4 rounded-md flex-1 flex gap-4">
                         <div className="w-1/3">
-                            <Image src="https://randomuser.me/api/portraits/men/10.jpg" alt="" width={144} height={144} className="w-36 h-36 rounded-full object-cover"></Image>
+                            <Image src="https://randomuser.me/api/portraits/men/10.jpg" alt="" width={144} height={144} className="w-36 h-36 rounded-full object-cover" />
                         </div>
                         <div className="w-2/3 flex flex-col justify-between gap-4">
                             <h1 className="text-xl font-semibold">Ruben Baptista</h1>
                             <p className="text-sm text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
                             <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                                    <Image src="/blood.png" alt="" width={14} height={14}>
-                                        <span>A+</span>
-                                    </Image>
+                                    <Image src="/blood.png" alt="" width={14} height={14} />
+                                    <span>A+</span>
                                 </div>
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                                    <Image src="/date.png" alt="" width={14} height={14}>
-                                        <span>January 2025</span>
-                                    </Image>
+                                    <Image src="/date.png" alt="" width={14} height={14} />
+                                    <span>January 2025</span>
                                 </div>
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                                    <Image src="/email.png" alt="" width={14} height={14}>
-                                        <span>user@gamil.com</span>
-                                    </Image>
+                                    <Image src="/email.png" alt="" width={14} height={14} />
+                                    <span>user@gamil.com</span>
                                 </div>
                                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
-                                    <Image src="/phone.png" alt="" width={14} height={14}>
-                                        <span>+351 960 032 846</span>
-                                    </Image>
+                                    <Image src="/phone.png" alt="" width={14} height={14} />
+                                    <span>+351 960 032 846</span>
                                 </div>
                             </div>
                         </div>
